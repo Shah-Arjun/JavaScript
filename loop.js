1e44c8e2d7488724d749d6799f182bb1d3dafb78
Arjun shah <arjunsah856@gmail.com>
2025-06-22 21:44:56 +0545

    loops in js

---
+++ loop.js
@@ -0,0 +1,26 @@
+//for loop
+// var i;
+// for (i = 0; i < 10; i++) {
+//     console.log("Hello World!");
+// }
+
+// const name = "Prakash Sarkari";
+// for (let i = 0; i < name.length; i++) {
+//     console.log(name[i]);
+// }
+
+//while loop
+var i = 1;
+while (i <= 10) {
+    console.log("Hello World!");
+    i++;
+}
+
+//do...while
+let ii = 0;
+do {
+    console.log(ii);
+    ii++;
+} while (ii < 10);
+
+//
