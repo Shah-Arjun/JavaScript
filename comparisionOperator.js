054f0601564e930b5827221cb4d5cac02b134f20
Arjun shah <arjunsah856@gmail.com>
2025-06-22 18:20:13 +0545

    comaprision operator in js

---
+++ comparisionOperator.js
@@ -0,0 +1,30 @@
+// //comparision operator
+// console.log(50 > 30);
+// console.log(30 >= 30);
+
+// //for stirng comapares ASCI value of characters
+// console.log("apple" > "banana");
+// console.log("glowing" > "glow");
+
+
+// console.log("2" > 1); //at first >operator converts the
+// console.log("01" == 1); // == operator compares only the number not its type
+
+// //strict comparision/equality
+// console.log("01" === 1); // === operator compares the number and its type
+
+
+// console.log(null == undefined); //true
+// console.log(null === undefined); //false
+
+
+// console.log(null > 0); //false
+// console.log(null < 0); //false
+// console.log(null == 0); //false
+// console.log(null === 0); //false
+
+
+console.log(null > 1); //false
+console.log(null < 1); // true
+
+console.log(null >= 1); // false
