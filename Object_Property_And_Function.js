929319ade63cefeaaf408b1597f79de4830c8c99
Arjun <arjunsah856@gmail.com>
2025-08-21 14:38:26 +0545

    computed property

--- Object_Property_And_Function.js
+++ Object_Property_And_Function.js
@@ -1,19 +1,34 @@
 // object
 const Example = {
-  Name: "Ram",              // property
+  Name: "Ram", // property
 
-  Func: function () {        //anonymus function   with the key as Func
+  //anonymus function   with the key as Func
+  Func: function () {
     console.log("hello");
   },
 
-  second() {               // normal function
+      // normal function
+  second() {
     console.log("GeeksForGeeks");
-  },
+  }
 };
-
-
-
 
 // accessing Func
 Example.Func();
-Example.second()
+Example.second();
+
+
+
+// ******computed property
+
+Example.age = 13;       // computed property , adding age property from outside
+
+//  Loop through keys
+for (let key in Example) {
+  console.log(key, ":", Example[key]);
+}
+
+// OR using Object.keys()
+Object.keys(Example).forEach((key) => {
+  console.log(key, ":", Example[key]);
+});
